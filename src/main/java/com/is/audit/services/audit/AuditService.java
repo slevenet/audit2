3594ba567e1fae367c283f207675742a20ef1402
SLevenets <l.sergei@it-platform.com>
2020-12-09 12:33:27 +0300

    ErrorHandling was added

--- src/main/java/com/is/audit/services/audit/AuditService.java
+++ src/main/java/com/is/audit/services/audit/AuditService.java
@@ -2,8 +2,11 @@ package com.is.audit.services.audit;
 
 import com.is.audit.model.Audit;
 import com.is.audit.model.AuditType;
+import com.sun.org.slf4j.internal.Logger;
+import com.sun.org.slf4j.internal.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.security.Principal;
@@ -15,20 +18,29 @@ import java.util.stream.Collectors;
 
 public abstract class AuditService {
 
+    Logger logger = LoggerFactory.getLogger(AuditService.class);
+
     public Audit audit(Object obj){
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        Collection<? extends GrantedAuthority> authorities =  authentication.getAuthorities();
-        List<String> roles = authorities.stream().map(x -> x.getAuthority()).collect(Collectors.toList());
-        Optional<Object> principal =  Optional.of(authentication.getPrincipal());
         Audit audit = createAudit(obj);
         audit.setEvent_ts(LocalDateTime.now().toString());
-        audit.setUser_roles(roles);
-
-        if(principal.get() instanceof String)
-            audit.setUsername((String) principal.get());
-        else audit.setUsername(((Principal) principal.get()).getName());
-
-        return audit;
+        try {
+            SecurityContext securityContext = SecurityContextHolder.getContext();
+            if(securityContext != null) {
+                Authentication authentication = securityContext.getAuthentication();
+                Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+                List<String> roles = authorities.stream().map(x -> x.getAuthority()).collect(Collectors.toList());
+                Optional<Object> principal = Optional.of(authentication.getPrincipal());
+                audit.setUser_roles(roles);
+                if (principal.get() instanceof String)
+                    audit.setUsername((String) principal.get());
+                else audit.setUsername(((Principal) principal.get()).getName());
+            }
+            return audit;
+        }
+        catch (Exception ex){
+            logger.error("Error audit", ex);
+            return audit;
+        }
     }
 
     abstract Audit createAudit(Object obj);
